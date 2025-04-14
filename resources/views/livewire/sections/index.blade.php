d20b1f3e815f1cb14e8ee1692462742ee8aab7f2
VikingoCH <ales.urizar@gmail.com>
2025-04-15 01:01:32 +0200

    Menu-Sections view

--- resources/views/livewire/sections/index.blade.php
+++ resources/views/livewire/sections/index.blade.php
@@ -5,19 +5,22 @@
             <x-buttons.add link="{{ route('sections.create') }}" responsive />
         </x-slot:actions>
     </x-header>
-    <x-card class="w-full rounded-xl border border-neutral-200 dark:border-neutral-700" separator shadow>
-        <div>
-            <x-table :headers="$headers" :rows="$menuSections" changeRowOrder empty-text="{{ __('Menu sections not found!') }}"
-                show-empty-text>
-                @scope('cell_orderIcon', $menuSection)
-                    <x-icon class="cursor-move text-gray-400" name="c-arrows-up-down" wire:sortable.handle />
-                @endscope
-                @scope('actions', $menuSection)
-                    <div class="flex flex-nowrap gap-3">
-                        <x-buttons.edit link="{{ route('sections.edit', $menuSection) }}" />
-                        <x-buttons.trash wire:click="delete({{ $menuSection->id }})" />
+
+    <div class="flex justify-center">
+        <x-card class="w-full rounded-xl border border-neutral-200 lg:w-3/4 dark:border-neutral-700" separator shadow>
+            <div>
+                <x-table :headers="$headers" :rows="$menuSections" changeRowOrder
+                    empty-text="{{ __('Menu sections not found!') }}" show-empty-text>
+                    @scope('cell_orderIcon', $menuSection)
+                        <x-icon class="cursor-move text-gray-400" name="c-arrows-up-down" wire:sortable.handle />
                     @endscope
-            </x-table>
-        </div>
-    </x-card>
+                    @scope('actions', $menuSection)
+                        <div class="flex flex-nowrap gap-3">
+                            <x-buttons.edit link="{{ route('sections.edit', $menuSection) }}" />
+                            <x-buttons.trash wire:click="delete({{ $menuSection->id }})" />
+                        @endscope
+                </x-table>
+            </div>
+        </x-card>
+    </div>
 </div>

9883aaf763e7551b772f74539c90f1eb147cafb0
VikingoCH <ales.urizar@gmail.com>
2025-04-15 01:04:20 +0200

    Menu-Sides view

--- resources/views/livewire/sides/create.blade.php
+++ resources/views/livewire/sides/create.blade.php
@@ -1,16 +1,19 @@
 <div class="flex h-full w-full flex-1 flex-col gap-2 rounded-xl">
     <x-header progress-indicator separator title="{{ __('New Side dish') }}" />
-    <x-card class="w-full rounded-xl border border-neutral-200 dark:border-neutral-700" separator shadow>
-        <x-form wire:submit="save">
-            <div class="flex flex-col gap-4 lg:flex-row lg:gap-8">
-                <div class="flex flex-col gap-4 lg:w-full">
-                    <x-input label="{{ __('labels.name') }}" wire:model.blur="name" />
+
+    <div class="flex justify-center">
+        <x-card class="w-full rounded-xl border border-neutral-200 lg:w-3/4 dark:border-neutral-700" separator shadow>
+            <x-form wire:submit="save">
+                <div class="flex flex-col gap-4 lg:flex-row lg:gap-8">
+                    <div class="flex flex-col gap-4 lg:w-full">
+                        <x-input label="{{ __('labels.name') }}" wire:model.blur="name" />
+                    </div>
                 </div>
-            </div>
-            <x-slot:actions separator>
-                <x-buttons.save spinner="save" type="submit" />
-                <x-buttons.cancel link="{{ route('sides.index') }}" />
-            </x-slot:actions>
-        </x-form>
-    </x-card>
+                <x-slot:actions separator>
+                    <x-buttons.save spinner="save" type="submit" />
+                    <x-buttons.cancel link="{{ route('sides.index') }}" />
+                </x-slot:actions>
+            </x-form>
+        </x-card>
+    </div>
 </div>
--- resources/views/livewire/sides/index.blade.php
+++ resources/views/livewire/sides/index.blade.php
@@ -5,15 +5,19 @@
             <x-buttons.add link="{{ route('sides.create') }}" responsive />
         </x-slot:actions>
     </x-header>
-    <x-card class="w-full rounded-xl border border-neutral-200 dark:border-neutral-700" separator shadow>
-        <div>
-            <x-table :headers="$headers" :rows="$menuSides" empty-text="{{ __('Side dishes not found!') }}" show-empty-text>
-                @scope('actions', $menuSide)
-                    <div class="flex flex-nowrap gap-3">
-                        <x-buttons.edit link="{{ route('sides.edit', $menuSide) }}" />
-                        <x-buttons.trash wire:click="delete({{ $menuSide->id }})" />
-                    @endscope
-            </x-table>
-        </div>
-    </x-card>
+
+    <div class="flex justify-center">
+        <x-card class="w-full rounded-xl border border-neutral-200 lg:w-3/4 dark:border-neutral-700" separator shadow>
+            <div>
+                <x-table :headers="$headers" :rows="$menuSides" empty-text="{{ __('Side dishes not found!') }}"
+                    show-empty-text>
+                    @scope('actions', $menuSide)
+                        <div class="flex flex-nowrap gap-3">
+                            <x-buttons.edit link="{{ route('sides.edit', $menuSide) }}" />
+                            <x-buttons.trash wire:click="delete({{ $menuSide->id }})" />
+                        @endscope
+                </x-table>
+            </div>
+        </x-card>
+    </div>
 </div>

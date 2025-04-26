0964f776274ecbb3da1f0cf09bc441731bc8d742
VikingoCH <ales.urizar@gmail.com>
2025-04-26 02:14:25 +0200

    transactions

--- app/Livewire/Transactions/Index.php
+++ app/Livewire/Transactions/Index.php
@@ -8,9 +8,29 @@ use Livewire\Component;
 class Index extends Component
 {
 
+    public $orders = [];
+    public array $expanded = [];
+
+    public function mount()
+    {
+        $this->orders = Order::with('transactions')->get();
+    }
+
+    public function headers(): array
+    {
+        return [
+            // ['key' => 'id', 'label' => '#'],
+            ['key' => 'number', 'label' => __('labels.number')],
+            ['key' => 'total', 'label' => __('labels.total'), 'format' => ['currency', '2.\'', 'CHF '], 'class' => 'text-center'],
+            ['key' => 'created_at', 'label' => __('labels.date')],
+        ];
+    }
+
 
     public function render()
     {
-        return view('livewire.transactions.index');
+        return view('livewire.transactions.index', [
+            'headers' => $this->headers(),
+        ]);
     }
 }
--- resources/views/livewire/transactions/index.blade.php
+++ resources/views/livewire/transactions/index.blade.php
@@ -1,3 +1,22 @@
-<div>
-    {{-- Success is as dangerous as failure. --}}
+<div class="flex h-full w-full flex-1 flex-col gap-2 rounded-xl">
+    <x-header progress-indicator separator subtitle="{{ __('Configure the sections of the menu') }}"
+        title="{{ __('labels.transanctions') }}">
+    </x-header>
+
+    <div class="flex flex-col-reverse justify-center gap-4 lg:flex-row">
+        <x-card class="w-full rounded-xl border border-neutral-200 lg:w-3/4 dark:border-neutral-700" separator shadow>
+            <x-table :headers="$headers" :rows="$orders" empty-text="{{ __('Transactions not found!') }}" expandable
+                show-empty-text wire:model="expanded">
+                @scope('expansion', $order)
+                    @foreach ($order->transactions as $transaction)
+                        <div class="flex flex-row gap-4 border-y border-gray-300 p-2">
+                            <x-icon class="m-4 text-green-600" name="gmdi.check-box-o" />
+                            <span class="border-r border-gray-400 p-4">{{ $transaction->number }}</span>
+                            <span class="p-4">{{ 'CHF ' . number_format($transaction->total, 2) }}</span>
+                        </div>
+                    @endforeach
+                @endscope
+            </x-table>
+        </x-card>
+    </div>
 </div>
